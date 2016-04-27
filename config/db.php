622004b42502056d0d8b209780e0a56fc3783652
Twist-tracer <twist.tracer@gmail.com>
2016-04-27 06:41:12 +0300

    Change password and dbName

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=gb_calendar',
     'username' => 'root',
-    'password' => '',
+    'password' => 'Myphp2014',
     'charset' => 'utf8',
 ];
